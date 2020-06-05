c8adf39168f84c014208e6394bf94b6f8de3f8be
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-05 17:26:21 +0100

    test(feat): evaluate addtion

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,5 +1,16 @@
 const calculator = sum => {
-  const evaluate = () => sum;
+  const evaluate = () => {
+    const operator = sum.split(' ')[1];
+    const output = [sum];
+
+    if (operator === '+') {
+      output.push(
+        Number(sum.split(' ')[0]) +
+          Number(sum.split(' ')[sum.split(' ').length - 1])
+      );
+    }
+    return output;
+  };
   return evaluate();
 };
 
--- Calculator/calculator.test.js
+++ Calculator/calculator.test.js
@@ -1,7 +1,9 @@
 const calculator = require('./calculator');
 
 describe('calculator', () => {
-  it('takes in a sum and returns it', () => {
-    expect(calculator('1 + 1')).toEqual('1 + 1');
+  describe('when the sum is addition', () => {
+    it('takes in a sum of 1 + 1 and returns the sum with the answer, 2', () => {
+      expect(calculator('1 + 1')).toEqual(['1 + 1', 2]);
+    });
   });
 });
