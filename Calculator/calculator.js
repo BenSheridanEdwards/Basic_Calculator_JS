75b75249107c6ebec6a7f9ed5aaa6c9b2a753264
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-05 17:34:06 +0100

    test(feat): evaluate subtraction

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -2,12 +2,18 @@ const calculator = sum => {
   const evaluate = () => {
     const operator = sum.split(' ')[1];
     const output = [sum];
+    const firstNumber = Number(sum.split(' ')[0]);
+    const secondNumber = Number(sum.split(' ')[sum.split(' ').length - 1]);
 
-    if (operator === '+') {
-      output.push(
-        Number(sum.split(' ')[0]) +
-          Number(sum.split(' ')[sum.split(' ').length - 1])
-      );
+    switch (operator) {
+      case '+':
+        output.push(firstNumber + secondNumber);
+        break;
+      case '-':
+        output.push(firstNumber - secondNumber);
+        break;
+      default:
+        break;
     }
     return output;
   };
--- Calculator/calculator.test.js
+++ Calculator/calculator.test.js
@@ -2,8 +2,13 @@ const calculator = require('./calculator');
 
 describe('calculator', () => {
   describe('when the sum is addition', () => {
-    it('takes in a sum of 1 + 1 and returns the sum with the answer, 2', () => {
+    it('takes in a sum of 1 + 1 and returns the sum with the correct answer, 2', () => {
       expect(calculator('1 + 1')).toEqual(['1 + 1', 2]);
+    });
+  });
+  describe('when the sum is subtraction', () => {
+    it('takes in a sum of 5 - 1 and returns the sum with the correct answer, 4', () => {
+      expect(calculator('5 - 1')).toEqual(['5 - 1', 4]);
     });
   });
 });
